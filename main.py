8138766e1ff17e9285a11a38983cc398c32edfb1
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 09:33:22 +0200

    feat: declare variables for arranged_problems and the parts, initial state is empty string

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ def arithmetic_arranger(problems, show_answers=False):
     if len(problems) > 5:
         return "Error: Too many problems."
     
+    arranged_problems = ""
+    line1 = ""
+    line2 = ""
+    dashes = ""
+    answers = ""
+    
+    
+    
     return arithmetic_arranger
