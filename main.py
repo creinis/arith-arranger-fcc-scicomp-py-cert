38b56bf6ce4ce084a2693d94cd29a6e1eb5d1ca9
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 09:29:43 +0200

    feat: check number of problemas, based on rules: If there are too many problems supplied to the function. The limit is five, anything more will return: **Error: Too many problems.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def arithmetic_arranger(problems, show_answers=False):
     
+    # Check the number of problems
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    
     return arithmetic_arranger
